7b6dd74e44ae52bcd00f1c99f731eebde1bb2121
布小沫 <95112082@qq.com>
2017-10-08 02:38:58 +0800

    update shell and dockerfile

---
+++ install_docker.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+curl -L https://github.com/docker/compose/releases/download/1.16.1/docker-compose-`uname -s`-`uname -m` -o /usr/local/bin/docker-compose
+chmod +x /usr/local/bin/docker-compose
+curl -L https://raw.githubusercontent.com/docker/compose/1.16.1/contrib/completion/bash/docker-compose -o /etc/bash_completion.d/docker-compose
