c66526b4abe4b715ae710e6ab64ca0bad2763c2e
布小沫 <95112082@qq.com>
2017-10-12 17:57:10 +0800

    添加ubuntu内核更新命令

--- kernel_update.sh
+++ kernel_update.sh
@@ -2,12 +2,17 @@
 
 # import key
 rpm --import https://www.elrepo.org/RPM-GPG-KEY-elrepo.org
-
 # install elrepo repo
 rpm -Uvh http://www.elrepo.org/elrepo-release-7.0-2.el7.elrepo.noarch.rpm
-
 # install kernel
 yum --enablerepo=elrepo-kernel install kernel-lt kernel-lt-headers kernel-lt-devel -y
 
 # modify grub
 grub2-set-default 0
+KERNEL_VERSION=
+wget http://kernel.ubuntu.com/~kernel-ppa/mainline/v4.9-rc8/linux-image-4.9.0-040900rc8-generic_4.9.0-040900rc8.201612051443_amd64.deb
+dpkg -i linux-image-4.9.0*.deb
+dpkg -l|grep linux-image
+sudo apt-get remove linux-image-[Tab补全]
+update-grub
+reboot
