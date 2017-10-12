2afa4716592c46dc272a68b8f18623ae2f0d1099
布小沫 <95112082@qq.com>
2017-10-12 10:22:10 +0800

    添加内核更新脚本

--- install_docker.sh
+++ install_docker.sh
@@ -9,6 +9,14 @@ apt-get install linux-image-extra-$(uname -r) linux-image-extra-virtual -y
 apt-get install linux-image-generic-lts-xenial -y
 apt-get install docker-engine -y
 
+apt-get install apt-transport-https ca-certificates curl gnupg2 software-properties-common -y
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+sudo add-apt-repository \
+   "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
+   $(lsb_release -cs) \
+   stable"
+apt-get install docker-ce
+
 # vim /etc/docker/daemon.json
 
 Aliyun_Quicken='https://i3jtbyvy.mirror.aliyuncs.com'
