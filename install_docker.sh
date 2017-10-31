5b5684e40633a1c790f0f252663c55f69e9d7d40
布小沫 <95112082@qq.com>
2017-11-01 00:41:49 +0800

    no message

--- install_docker.sh
+++ install_docker.sh
@@ -1,35 +1,57 @@
 #!/bin/bash
-# docker-engine
-apt-get update
-apt-get dist-upgrade -y
-apt-get install apt-transport-https ca-certificates -y
-apt-key adv --keyserver hkp://p80.pool.sks-keyservers.net:80 --recv-keys 58118E89F3A912897C070ADBF76221572C52609D
-echo "deb https://mirrors.tuna.tsinghua.edu.cn/docker/apt/repo ubuntu-xenial main" > /etc/apt/sources.list.d/docker.list
-apt-get update
-apt-get install linux-image-extra-$(uname -r) linux-image-extra-virtual -y
-apt-get install linux-image-generic-lts-xenial -y
-apt-get install docker-engine -y
+whereis yum &> /dev/null
+if [ $? == 0 ];
+    CMD=yum
+else
+    CMD=apt
+fi
+case ${CMD} in
+    yum )
+        yum install -y yum-utils device-mapper-persistent-data lvm2 curl
+        yum-config-manager --add-repo http://mirrors.aliyun.com/docker-ce/linux/centos/docker-ce.repo
+        yum -y install docker-ce
+    ;;
+    apt )
+        apt-get install apt-transport-https ca-certificates curl gnupg2 software-properties-common -y
+        curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+        sudo add-apt-repository \
+           "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
+           $(lsb_release -cs) \
+           stable"
+        apt-get update
+        apt-get install docker-ce -y
+    ;;
+esac
 
-# docker-ce
-apt-get install apt-transport-https ca-certificates curl gnupg2 software-properties-common -y
-curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
-sudo add-apt-repository \
-   "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
-   $(lsb_release -cs) \
-   stable"
-apt-get update
-apt-get install docker-ce -y
-
-vim /etc/docker/daemon.json
+cat > /etc/docker/daemon.json << EOF
 {
-  "registry-mirrors" : [
-    "https://i3jtbyvy.mirror.aliyuncs.com"
-  ],
-  "debug" : true,
-  "experimental" : true
+"registry-mirrors" : [
+"https://i3jtbyvy.mirror.aliyuncs.com"
+],
+"debug" : true,
+"experimental" : true
 }
-systemctl restart docker
+EOF
 # docker-compose
 curl -L https://github.com/docker/compose/releases/download/1.16.1/docker-compose-`uname -s`-`uname -m` -o /usr/local/bin/docker-compose
 chmod +x /usr/local/bin/docker-compose
 curl -L https://raw.githubusercontent.com/docker/compose/1.16.1/contrib/completion/bash/docker-compose -o /etc/bash_completion.d/docker-compose
+
+# # SSR
+# sed -i '/net.core.default_qdisc/d' /etc/sysctl.conf
+# sed -i '/net.ipv4.tcp_congestion_control/d' /etc/sysctl.conf
+# echo "net.core.default_qdisc = fq" >> /etc/sysctl.conf
+# echo "net.ipv4.tcp_congestion_control = bbr" >> /etc/sysctl.conf
+# sysctl -p
+
+
+# # docker-engine
+# apt-get update
+# apt-get dist-upgrade -y
+# apt-get install apt-transport-https ca-certificates -y
+# apt-key adv --keyserver hkp://p80.pool.sks-keyservers.net:80 --recv-keys 58118E89F3A912897C070ADBF76221572C52609D
+# echo "deb https://mirrors.tuna.tsinghua.edu.cn/docker/apt/repo ubuntu-xenial main" > /etc/apt/sources.list.d/docker.list
+# apt-get update
+# apt-get install linux-image-extra-$(uname -r) linux-image-extra-virtual -y
+# apt-get install linux-image-generic-lts-xenial -y
+# apt-get install docker-engine -y
--- kernel_update.sh
+++ kernel_update.sh
@@ -1,27 +1,27 @@
 #!/bin/bash
-# CentOS
-# import key
-rpm --import https://www.elrepo.org/RPM-GPG-KEY-elrepo.org
-# install elrepo repo
-rpm -Uvh http://www.elrepo.org/elrepo-release-7.0-2.el7.elrepo.noarch.rpm
-# install kernel
-yum --enablerepo=elrepo-kernel install kernel-lt kernel-lt-headers kernel-lt-devel -y
-
-# modify grub
-# grub2-set-default 0
-# KERNEL_VERSION=
-
-# Ubuntu
-wget http://kernel.ubuntu.com/~kernel-ppa/mainline/v4.10/linux-image-4.10.0-041000-generic_4.10.0-041000.201702191831_amd64.deb
-dpkg -i linux-image-4.10.0
-dpkg -l|grep linux-image
-sudo apt-get remove linux-image-[Tab补全]
-update-grub
+whereis yum &> /dev/null
+if [ $? == 0 ];
+    CMD=yum
+else
+    CMD=apt
+fi
+case ${CMD} in
+    yum )
+        rpm --import https://www.elrepo.org/RPM-GPG-KEY-elrepo.org
+        rpm -Uvh http://www.elrepo.org/elrepo-release-7.0-2.el7.elrepo.noarch.rpm
+        yum --enablerepo=elrepo-kernel install kernel-lt kernel-lt-headers kernel-lt-devel -y
+    ;;
+    apt )
+        apt-get install wget -y
+        wget http://kernel.ubuntu.com/~kernel-ppa/mainline/v4.10/linux-image-4.10.0-041000-generic_4.10.0-041000.201702191831_amd64.deb
+        dpkg -i linux-image-4.10.0-041000-generic_4.10.0-041000.201702191831_amd64.deb
+        update-grub
+    ;;
+esac
 reboot
-
-# SSR
-sed -i '/net.core.default_qdisc/d' /etc/sysctl.conf
-sed -i '/net.ipv4.tcp_congestion_control/d' /etc/sysctl.conf
-echo "net.core.default_qdisc = fq" >> /etc/sysctl.conf
-echo "net.ipv4.tcp_congestion_control = bbr" >> /etc/sysctl.conf
-sysctl -p
+# # SSR
+# sed -i '/net.core.default_qdisc/d' /etc/sysctl.conf
+# sed -i '/net.ipv4.tcp_congestion_control/d' /etc/sysctl.conf
+# echo "net.core.default_qdisc = fq" >> /etc/sysctl.conf
+# echo "net.ipv4.tcp_congestion_control = bbr" >> /etc/sysctl.conf
+# sysctl -p
