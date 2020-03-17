a03512381d98941dc50a8f93197055b251d86c0b
buxiaomo <95112082@qq.com>
2020-03-17 19:55:03 +0800

    add v2ray+nginx

---
+++ v2ray.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+apt install curl gnupg2 ca-certificates lsb-release -y
+curl -s http://nginx.org/keys/nginx_signing.key | apt-key add -
+cat > /etc/apt/sources.list.d/nginx.list << EOF
+deb http://nginx.org/packages/$(. /etc/os-release; echo "$ID")/ $(lsb_release -cs) nginx
+deb-src http://nginx.org/packages/$(. /etc/os-release; echo "$ID")/ $(lsb_release -cs) nginx
+EOF
+apt-get update
+apt-get install nginx -y
+wget https://raw.githubusercontent.com/buxiaomo/v2ray/master/nginx.conf -O /etc/nginx/nginx.conf
+rm -rf /etc/nginx/conf.d/default.conf
+bash <(curl -L -s https://install.direct/go.sh)
