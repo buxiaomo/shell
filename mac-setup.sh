0e4e8aece5fb717a57f542fcd365a6d821f908b0
布小沫 <95112082@qq.com>
2021-08-21 15:29:06 +0800

    Update mac-setup.sh

--- mac-setup.sh
+++ mac-setup.sh
@@ -9,31 +9,34 @@ sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.
 brew install --cask google-chrome
 brew install --cask zoomus
 brew install --cask iterm2
-brew install --cask docker
-brew install --cask postman
 brew install --cask notion
-brew install --cask telegram-desktop
-brew install --cask shiftit
-brew install --cask visual-studio-code
-brew install --cask sourcetree
-brew install --cask cheatsheet
-brew install --cask dash
+
 brew install --cask microsoft-outlook
 brew install --cask microsoft-powerpoint
 brew install --cask microsoft-word
 brew install --cask microsoft-auto-update
+
+brew install --cask sourcetree
 brew install --cask pycharm
 brew install --cask goland
+brew install --cask visual-studio-code
+brew install --cask docker
+brew install --cask postman
+
 brew install --cask qq
 brew install --cask wechat
 brew install --cask wechatwork
-brew install --cask qqmusic
-brew install --cask qqlive
+brew install --cask telegram-desktop
+
 brew install --cask bartender
 brew install --cask istat-menus
 brew install --cask tunnelblick
 brew install --cask balenaetcher
 brew install --cask parallels
+
+brew install --cask qqmusic
+brew install --cask qqlive
+brew install --cask qiyimedia
 
 # Install tools
 brew install wget telnet curl zsh terraform go python@3 node kubernetes-cli
